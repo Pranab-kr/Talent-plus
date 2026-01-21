6d8abbf7427036e9c7c8284b8f70ebfdda527b6c
pranab-kr <pranabm406@gmail.com>
2026-01-22 00:31:19 +0530

    session

--- backend/src/controllers/session.cotroller.js
+++ backend/src/controllers/session.cotroller.js
@@ -1,5 +1,61 @@
-import { streamClient } from "../lib/stream.js";
+import { Session } from "../model/Session.js";
+import { chatClient, streamClient } from "../lib/stream.js";
 
-export const createSession = async (req, res) => {};
+export const createSession = async (req, res) => {
+  try {
+    const { problem, deficulty } = req.body;
+
+    const userId = req.user._id;
+    const clerkId = req.user.clerkId;
+
+    if (!problem || !deficulty) {
+      return res
+        .status(400)
+        .json({ error: "Problem and difficulty are required" });
+    }
+
+    const callId = `session_${Date.now()}_${Math.random().toString(36).substring(7)}`;
+
+    const session = await Session.create({
+      problem,
+      deficulty,
+      host: userId,
+      callId,
+    });
+
+    //create stream video call;
+    await streamClient.video.call("default", callId).getOrCreate({
+      data: {
+        created_by: clerkId,
+        custiom: {
+          problem,
+          deficulty,
+          sessionId: session._id.toString(),
+        },
+      },
+    });
+
+    const channel = chatClient.channel("messaging", callId, {
+      name: `Session Chat - ${problem}`,
+      created_by_id: clerkId,
+      members: [clerkId],
+    });
+
+    await channel.create();
+
+    res.status(201).json({ session });
+  } catch (err) {
+    console.error("Error creating session:", err);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
 
 export const getActiveSessions = async (req, res) => {};
+
+export const getMyRecentSessions = async (req, res) => {};
+
+export const getSessionById = async (req, res) => {};
+
+export const joinSession = async (req, res) => {};
+
+export const endSession = async (req, res) => {};
--- backend/src/routes/session.route.js
+++ backend/src/routes/session.route.js
@@ -2,13 +2,23 @@ import { Router } from "express";
 import { protectedRoute } from "../middlewares/protectedRoute.js";
 import {
   createSession,
+  endSession,
   getActiveSessions,
+  getMyRecentSessions,
+  getSessionById,
+  joinSession,
 } from "../controllers/session.cotroller.js";
 
 const router = Router();
 
-router.post("/", protectedRoute, createSessionon);
+router.post("/", protectedRoute, createSession);
 
-router.get("/active", protectedRoute, getActiveSessionseSessions);
+router.get("/active", protectedRoute, getActiveSessions);
+
+router.get("my-recent", protectedRoute, getMyRecentSessions);
+
+router.get("/:id", protectedRoute, getSessionById);
+router.get("/:id/join", protectedRoute, joinSession);
+router.get("/:id/end", protectedRoute, endSession);
 
 export default router;
